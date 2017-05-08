eb2678a201dfc376f1fcc7db27ea56b61131ac34
Chris Manders <cmanders159@gmail.com>
2017-05-08 11:50:19 +0100

    Initial commit

---
+++ CloudOps Assistant/AlertWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace CloudOps_Assistant
+{
+    /// <summary>
+    /// Interaction logic for AlertWindow.xaml
+    /// </summary>
+    public partial class AlertWindow : Window
+    {
+        public AlertWindow(string headerText, string bodyText)
+        {
+            InitializeComponent();
+            
+            alertWindow_NotificationText_TextBlock.Inlines.Add(new Bold(new Run(headerText)));
+            alertWindow_NotificationText_TextBlock.Inlines.Add(new LineBreak());
+            alertWindow_NotificationText_TextBlock.Inlines.Add(new LineBreak());
+            alertWindow_NotificationText_TextBlock.Inlines.Add(new Run(bodyText));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+    }
+}
---
+++ CloudOps Assistant/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CloudOps_Assistant
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NotifyIcon ni = new NotifyIcon();
+            ni.Icon = new Icon(@"C:\Users\christopher.manders\desktop\Main.ico");
+            ni.Visible = true;
+            ni.DoubleClick +=
+                delegate (object sender, EventArgs args)
+                {
+                    Show();
+                    WindowState = WindowState.Normal;
+                };
+
+            notificationTestAsync();
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+                Hide();
+
+            base.OnStateChanged(e);
+        }
+
+        private async Task notificationTestAsync()
+        {
+            await Task.Delay(3000);
+
+            AlertWindow alertWindow = new AlertWindow("Hello", "world");
+            alertWindow.ShowDialog();
+        }
+    }
+}
